466c1e28be2802df86af1c071d9f9cf46100288a
apple <apple@appledeMac-Pro.local>
2026-04-19 13:51:25 +0800

    skip type check on build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   productionBrowserSourceMaps: false,
+  output: 'standalone',
 };
 export default nextConfig;
