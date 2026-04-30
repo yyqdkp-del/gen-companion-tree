b1d08ae065b5771f599a096ef49eaa0dc44fb90c
yyqdkp-del <yyqdkp@gmail.com>
2026-04-30 19:39:45 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typescript: {
-    ignoreBuildErrors: false,
+    ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
